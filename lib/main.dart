1385a189780cc10e8c9a57305833636a6b8c616d
Agnt45 <an160264@gmail.com>
2019-01-29 00:43:21 +0530

    The Created Basic UI

---
+++ lib/main.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'T0_d0',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final topAppBar = AppBar(
+    elevation: 0.1,
+    backgroundColor: Colors.black87,
+    title: Text(
+      "T0_d0",
+      style: TextStyle(
+        color: Colors.white70,
+      ),
+    ),
+    actions: <Widget>[
+      IconButton(
+        icon: Icon(Icons.menu),
+        onPressed: () {},
+      )
+    ],
+  );
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black87,
+      appBar: topAppBar,
+      body: Container(
+        child: ListView.builder(
+          scrollDirection: Axis.vertical,
+          shrinkWrap: true,
+          itemCount: 10,
+          itemBuilder: (BuildContext context, int index) {
+            return Card(
+              elevation: 8.0,
+              margin: new EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
+              child: Container(
+                decoration:
+                    BoxDecoration(color: Color.fromRGBO(64, 75, 96, .9)),
+                child: ListTile(
+                    contentPadding:
+                        EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+                    title: Text(
+                      "Introduction to Driving",
+                      style: TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.bold),
+                    ),
+                    trailing: Icon(Icons.keyboard_arrow_right,
+                        color: Colors.white, size: 30.0)),
+              ),
+            );
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.grey.shade900,
+        tooltip: 'Add Todo',
+        child: Icon(
+          Icons.add,
+        ),
+      ), 
+    );
+  }
+}
